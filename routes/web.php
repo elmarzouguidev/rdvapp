9f753ecaabc87421ecdac7be031530d2967e150e
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-06 16:00:35 +0100

    update some stuffs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Book\BookController;
+use App\Http\Controllers\DevController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,25 @@ Route::get('/', function () {
 
 Route::get('/', [BookController::class, 'index'])->name('book');
 Route::post('/', [BookController::class, 'store'])->name('book.store');
+
+
+
+Route::group(['prefix' => 'app'], function () {
+
+    Route::get('/link', [DevController::class, 'storageLink']);
+    Route::get('/unlink', [DevController::class, 'storageUnLink']);
+
+    Route::get('/migrate', [DevController::class, 'migrateAll']);
+
+    Route::get('/migrate-fresh', [DevController::class, 'migrateFresh']);
+
+    Route::get('/seed', [DevController::class, 'migrateSeed']);
+
+    Route::get('/cache', [DevController::class, 'cacheAll']);
+    Route::get('/clear', [DevController::class, 'cleareAll']);
+
+    Route::get('/app-up', [DevController::class, 'appUp']);
+    Route::get('/app-down', [DevController::class, 'appDown']);
+
+    Route::get('/site-map', [DevController::class, 'siteMap']);
+});
