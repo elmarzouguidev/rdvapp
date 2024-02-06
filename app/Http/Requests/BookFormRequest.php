c8d3c68756e99482a917efc99bc5de432c3e3ddb
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-06 16:13:18 +0100

    update some stuffs im mail -v2

--- app/Http/Requests/BookFormRequest.php
+++ app/Http/Requests/BookFormRequest.php
@@ -27,7 +27,7 @@ class BookFormRequest extends FormRequest
             'address' => ['required', 'string'],
             'type' => ['required', 'uuid', 'exists:types,uuid'],
             'city' => ['required', 'uuid', 'exists:cities,uuid'],
-            'book_date' => ['required', 'date', 'after:today'],
+            'book_date' => ['required', 'date', 'after_or_equal:today'],
             'book_time' => ['required', 'date_format:H:i']
         ];
     }
@@ -36,6 +36,7 @@ class BookFormRequest extends FormRequest
     {
         return [
             'book_date.after' => "la date de réservation doit être après aujourd'hui",
+            'book_date.after_or_equal' => "la date de réservation doit être aujourd'hui ou  après aujourd'hui",
         ];
     }
 }
